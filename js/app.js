7a5086576148dde483862433313a7a7f9e95b4c3
LEKDUP <lekdup@live.fr>
2022-09-12 23:17:06 +0200

    javascript edited

--- js/app.js
+++ js/app.js
@@ -3,38 +3,47 @@ let card_content_2 = document.querySelector('.card__component-2');
 let ratingBtns = document.querySelector('.rate-btn');
 let score = document.querySelector('.score');
 let ratingSubmitContainerEl = document.querySelector('.rate-submit-container');
+const submitBtn = document.querySelector(".submit-rating");
 
-
-ratingSubmitContainerEl.addEventListener('click', (e) => {
-    const button = e.target;
-
-    if (!button) {
-        return;
-    }
-
-    if (button.matches('.rate-btn')){
-        ratingBtns.forEach((btn) => {
-            btn.dataset.rating = '';
-        });
-        button.dataset.rating = button.value;
-    }
-})
-
-
-
-
-
-
-submit_btn.addEventListener('click', onSubmit);
-ratingBtns.forEach( btn => {
-    btn.addEventListener('click', handleRatingBtnClick);
-})
-
-function onSubmit(){
-    card_content_1.classList.add('hide');
+submitBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    card_content_1.classList.add ('hide');
     card_content_2.classList.remove('hide');
+});
+
+
+
+
+// ratingSubmitContainerEl.addEventListener('click', (e) => {
+//     const button = e.target;
+
+//     if (!button) {
+//         return;
+//     }
+
+//     if (button.matches('.rate-btn')){
+//         ratingBtns.forEach((btn) => {
+//             btn.dataset.rating = '';
+//         });
+//         button.dataset.rating = button.value;
+//     }
+// })
+
+
+
+
+
+
+// submit_btn.addEventListener('click', onSubmit);
+// ratingBtns.forEach( btn => {
+//     btn.addEventListener('click', handleRatingBtnClick);
+// })
+
+// function onSubmit(){
+//     card_content_1.classList.add('hide');
+//     card_content_2.classList.remove('hide');
     // console.log('submit click');
-}
+// }
 
 // function handleRatingBtnClick(event){
 //     ratingBtns.forEach( btn => {
