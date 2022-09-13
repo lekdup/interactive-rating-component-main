2c311b1c1d47459508e3dd700eaef34b94bf694c
LEKDUP <lekdup@live.fr>
2022-09-13 11:20:00 +0200

    done with first JS project

--- js/app.js
+++ js/app.js
@@ -1,61 +1,21 @@
-const card_content_1 = document.querySelector('.card__component-1');
-const card_content_2 = document.querySelector('.card__component-2');
-let ratingSubmitContainerEl = document.querySelector('.rate-submit-container');
+const card1 = document.querySelector('.component-1');
+const card2 = document.querySelector('.component-2');
 const submitBtn = document.querySelector(".submit-rating");
 
 const ratingBtns = document.querySelectorAll('.rate-btn');
 const score = document.querySelector('.score');
 
+// press the button and make the card1 disappear
 submitBtn.addEventListener("click", function(e){
     e.preventDefault();
-    card_content_1.classList.add ('hide');
-    card_content_2.classList.remove('hide');
+    card1.classList.add ('hide');
+    card2.classList.remove('hide');
 });
 
+// show the score in card2
 ratingBtns.forEach((rate) => {
     rate.addEventListener("click", ()=>{
         console.log(rate.innerHTML);
+        score.innerHTML = rate.innerHTML;
     })
 })
-
-
-
-// ratingSubmitContainerEl.addEventListener('click', (e) => {
-//     const button = e.target;
-
-//     if (!button) {
-//         return;
-//     }
-
-//     if (button.matches('.rate-btn')){
-//         ratingBtns.forEach((btn) => {
-//             btn.dataset.rating = '';
-//         });
-//         button.dataset.rating = button.value;
-//     }
-// })
-
-
-
-
-
-
-// submit_btn.addEventListener('click', onSubmit);
-// ratingBtns.forEach( btn => {
-//     btn.addEventListener('click', handleRatingBtnClick);
-// })
-
-// function onSubmit(){
-//     card_content_1.classList.add('hide');
-//     card_content_2.classList.remove('hide');
-    // console.log('submit click');
-// }
-
-// function handleRatingBtnClick(event){
-//     ratingBtns.forEach( btn => {
-//         btn.classList.remove('active');
-//     });
-//     event.target.classList.add('active');
-// }
-
-// console.log(ratingBtns);
